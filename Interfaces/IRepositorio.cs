52e9a8dd0c568f1f26589cf7fb04d9357c1229e5
Eklezyarez <eklezyarez@gmail.com>
2022-07-27 10:07:49 -0300

    create inteface

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_prj_cadserie.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+
+        T RetornaPorId(int id);
+
+        void Insere(Task entidade);
+
+        void Exclui(int id);
+
+        void Atualiza(int id, T entidade);
+
+        int ProximoId();
+    }
+}
