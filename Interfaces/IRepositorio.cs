9f00f21ad1bdd6ae6b89ae7cc12e6e644cc11a12
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-11-07 06:24:43 -0300

    Update IRepositorio.cs

--- Interfaces/IRepositorio.cs
+++ Interfaces/IRepositorio.cs
@@ -8,7 +8,6 @@ namespace dio_prj_cadserie.Interfaces
     public interface IRepositorio<T>
     {
         List<T> Lista();
-
         T RetornaPorId(int id);
         void Insere(Task entidade);
         void Exclui(int id);
